b7b5ea8fa7ad438bf2276572408c9b48e3221ad0
Alexandra Levina <93614091+Drumlex17@users.noreply.github.com>
2025-09-05 22:15:04 +0300

    added demoqa test

--- conftest.py
+++ conftest.py
@@ -1,11 +1,34 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
 
 
 @pytest.fixture(scope="function", autouse=True)
-def configuring_browser():
+def configuring_browser(setup_browser):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.timeout = 10
-    yield
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
+    yield browser
+
     browser.quit()
