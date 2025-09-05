5ad90d4478b990f0104638ec8f8ac7bb354ffec7
Alexandra Levina <93614091+Drumlex17@users.noreply.github.com>
2025-09-05 22:28:40 +0300

    added demoqa test and new branch

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ def configuring_browser(setup_browser):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.timeout = 10
+    yield
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
